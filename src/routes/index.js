eab02f358809b65770dbff9d21df30ac8288c8ab
Gustavo <graf.sousa@gmail.com>
2019-12-03 22:10:20 -0300

    Adiciona feature de configurações de rotas

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import { Dashboard } from '../pages/Dashboard/index';
+import { Profile } from '../pages/Profile/index';
+import { SignIn } from '../pages/SignIn/index';
+import { SignUp } from '../pages/SignUp/index';
+
+function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} />
+    </Switch>
+  );
+}
+
+export { Routes };
