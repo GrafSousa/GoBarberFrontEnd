009976f8770817e9b471e646cdf350b280fc1bce
Gustavo <graf.sousa@gmail.com>
2019-12-16 07:49:05 -0300

    Adiciona feature de armazenar o perfil

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import { authReducer } from './auth/reducer';
+import { user } from './user/reducers';
 
 const rootReducer = combineReducers({
   authReducer,
+  user,
 });
 
 export { rootReducer };
